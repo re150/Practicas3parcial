d2c05940046c922079224707b4aa4f986197f053
re150 <a21110175@live.ceti.mx>
2023-11-22 20:14:37 -0600

    second commit

--- LIbreria3D/src/Main.java
+++ LIbreria3D/src/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             Pixel g = new Pixel();
-           // g.  cubeProyection(200, 100, 5, 400, 400, 40, Color.BLACK);
             new Thread(g).start();
         });
     }
--- LIbreria3D/src/Pixel.java
+++ LIbreria3D/src/Pixel.java
@@ -12,9 +12,8 @@ public class Pixel extends JPanel implements Runnable {
         private BufferedImage buffer;
         private BufferedImage fondo;
         private Graphics fondoG;
-        private int viewX, viewY, viewZ;
-        private Boolean checkExistance = false;
-        private Key MyKeyInstance;
+        private int viewX = 5 , viewY = 20 , viewZ = -20;
+
 
     public Pixel() {
         init();
@@ -22,65 +21,79 @@ public class Pixel extends JPanel implements Runnable {
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
         setDoubleBuffered(true);
-        MyKeyInstance = new Key();
-        addKeyListener(MyKeyInstance);
+
+        addKeyListener(new KeyAdapter() {
+             @Override
+                 public void keyPressed(KeyEvent e) {
+                    int key = e.getKeyCode();
+                    if (key == KeyEvent.VK_LEFT) {
+                        viewX -=5;
+                    }
+                    if (key == KeyEvent.VK_RIGHT) {
+                        viewX += 5;
+                    }
+                    if (key == KeyEvent.VK_UP) {
+                        viewY -= 5;
+                    }
+                    if (key == KeyEvent.VK_DOWN) {
+                        viewY +=5;
+                    }
+                    if (key == KeyEvent.VK_Z ){
+                        viewZ += 5;
+                    }
+                    if (key == KeyEvent.VK_X ){
+                        viewZ -= 5;
+                    }
+                }
+        });
     }
 
     public void init () {
         setSize(700, 700);
         setVisible(true);
+
         buffer = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
         fondo = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
         fondoG = fondo.getGraphics();
-        fondoG.setColor(Color.white);
+        fondoG.setColor(Color.black);
         fondoG.fillRect(0, 0, getWidth(), getWidth());
-            JFrame frame = new JFrame("Pixel");
-            frame.setResizable(false);
-            frame.add(this);
-            frame.setSize(this.getWidth(), this.getHeight());
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
+
+        JFrame frame = new JFrame("Pixel");
+        frame.setResizable(false);
+        frame.add(this);
+        frame.setSize(this.getWidth(), this.getHeight());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 
 
     @Override
     public void update (Graphics g){
-            clear();
-            setBack(MyKeyInstance.GetX(), MyKeyInstance.GetY(), MyKeyInstance.GetZ());
-            cubeProyection(300, 100, 0, 500, 400, 5, Color.BLACK);
-            g.drawImage(fondo, 0, 0, this);
+        g.drawImage(fondo, 0, 0, this);
     }
 
     @Override
     public void paint (Graphics g){
-        if (checkExistance == false) {
-            buffer = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
-            buffer.setRGB(0, 0, Color.blue.getRGB());
-            g.drawImage(buffer, getWidth() / 2, getHeight() / 2, this);
-            checkExistance = Boolean.TRUE;
-            //update(g);
-        }
-          update(g);
+        update(g);
         clear();
+        try {
+            cubeProyection(300, 100, 0, 500, 400, 5, Color.white);
+        }catch (Exception e){}
     }
 
     public void clear () {
-        fondoG.setColor(Color.white);
+        fondoG.setColor(Color.black);
         fondoG.fillRect(0, 0, getWidth(), getHeight());
         repaint();
     }
 
     public void putPixel ( int x, int y, Color c){
-        buffer.setRGB(0, 0, c.getRGB());
-        fondo.getGraphics().drawImage(buffer, x, y, this);
-        repaint();
+       buffer.setRGB(0, 0, c.getRGB());
+       fondo.getGraphics().drawImage(buffer, x, y, this);
+       repaint();
     }
-    public void setBack ( int x, int y, int z){
-        this.viewX = x;
-        this.viewY = y;
-        this.viewZ = z;
-    }
+
     public void AlgoritmoDDALine ( int x0, int y0, int x1, int y1, Color c){
         float dx = x1 - x0;
         float dy = y1 - y0;
@@ -107,20 +120,7 @@ public class Pixel extends JPanel implements Runnable {
             putPixel(Math.round(x), Math.round(y), c);
         }
     }
-    public void drawCuadrado ( int x, int y, int a, int l, Color c){
-
-        int x0 = x;
-        int x1 = x + l;
-        int y0 = y;
-        int y1 = y + a;
-
-        AlgoritmoDDALine(x0, y0, x0, y1, c);
-        AlgoritmoDDALine(x1, y0, x1, y1, c);
-        AlgoritmoDDALine(x0, y1, x1, y1, c);
-        AlgoritmoDDALine(x0, y0, x1, y0, c);
-
-    }
-    public void Cuadrado ( int x1, int y1, int x2, int y2, Color c){
+    public void drawCuadrado ( int x1, int y1, int x2, int y2, Color c){
         AlgoritmoDDALine(x1, y1, x2, y1, c);
         AlgoritmoDDALine(x1, y1, x1, y2, c);
         AlgoritmoDDALine(x2, y1, x2, y2, c);
@@ -134,8 +134,9 @@ public class Pixel extends JPanel implements Runnable {
         cubeOnePoints = oneProyection(x1, y1, z1, x2, y2, z2);
         cubeTwoPoints = twoProyection(x1, y1, z1, x2, y2, z2);
 
-        Cuadrado(cubeOnePoints[0].x, cubeOnePoints[0].y, cubeOnePoints[3].x, cubeOnePoints[3].y, c);
-        Cuadrado(cubeTwoPoints[0].x, cubeTwoPoints[0].y, cubeTwoPoints[1].x, cubeTwoPoints[1].y, c);
+        drawCuadrado(cubeOnePoints[0].x, cubeOnePoints[0].y, cubeOnePoints[3].x, cubeOnePoints[3].y, c);
+        drawCuadrado(cubeTwoPoints[0].x, cubeTwoPoints[0].y, cubeTwoPoints[1].x, cubeTwoPoints[1].y, c);
+
 
         for (int i = 0; i < 4; i++) {
             putPixel(cubeOnePoints[i].x, cubeOnePoints[i].y, c);
@@ -146,6 +147,7 @@ public class Pixel extends JPanel implements Runnable {
             else
                 AlgoritmoDDALine(cubeOnePoints[i].x, cubeOnePoints[i].y, cubeTwoPoints[0].x, cubeTwoPoints[0].y, c);
         }
+
     }
 
     private Point[] oneProyection ( int x1, int y1, int z1, int x2, int y2, int z2){
@@ -185,7 +187,6 @@ public class Pixel extends JPanel implements Runnable {
 
     private Point convert ( int x, int y, int z){
         int xTemp, yTemp;
-        System.out.println("x= " + x);
         xTemp = viewX - ((viewZ * (x - viewX)) / (z - viewZ));
         yTemp = viewY - ((viewZ * (y - viewY)) / (z - viewZ));
 
@@ -193,28 +194,15 @@ public class Pixel extends JPanel implements Runnable {
 
         return converted;
     }
+
     @Override
     public void run () {
-        boolean exit = false;
-        while (!exit) {
-            try {
-                if (GetChange()) {
-                    repaint();
-                    SetChange(false);
-                }
-                Thread.sleep(100);
-            } catch (InterruptedException ignored) {
-                exit = true;
-            }
+        try {
+            repaint();
+            Thread.sleep(100);
+        } catch (InterruptedException ignored) {
         }
     }
 
-    public boolean GetChange () {
-        return MyKeyInstance.GetChange();
-    }
-
-        public void SetChange ( boolean NewChange){
-        MyKeyInstance.SetChange(NewChange);
-    }
 
 }
